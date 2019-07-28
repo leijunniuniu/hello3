be2276f3c8379f843cd6cfdbc367b0b15ddf1290
leijunniuniu <53336442+leijunniuniu@users.noreply.github.com>
2019-07-28 11:56:38 +0800

    Update and rename src/main/java/com/czxy/demo/hello01.java to  src/ main/ java/ com/ czxy/ demo/hello01.java
    修改了

--- src/ main/ java/ com/ czxy/ demo/hello01.java
+++ src/ main/ java/ com/ czxy/ demo/hello01.java
@@ -5,6 +5,6 @@ public class hello01 {
     public static void main(String[] args) {
         System.out.println("maven01 第一个git项目");
 
-        System.out.println("maven02 第二次修改");
+        
     }
 }
